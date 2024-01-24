2ccc050876343aac7cc86ea0a4ed1e6902b6cf1f
Chansak <chansak_noi@hotmail.com>
2024-01-24 17:08:18 +0700

    init

---
+++ Constants/Constants.cs
@@ -0,0 +1,12 @@
+namespace Clients
+{
+    public class Constants
+    {
+        public const string Authority = "http://idsvr.smartcityclick.com:7280";
+        //public const string Authority = "https://localhost:5101";
+        public const string AuthorityMtls = "https://identityserver.local";
+
+        public const string SampleApi = "https://localhost:5005/";
+        public const string SampleApiMtls = "https://api.identityserver.local/";
+    }
+}
