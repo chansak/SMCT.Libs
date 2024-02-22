c2070a7cfa1fd81c16217dc3081b4afd4a504f45
Chansak <chansak_noi@hotmail.com>
2024-02-22 20:28:04 +0700

    code changing for release to server

--- Constants/Constants.cs
+++ Constants/Constants.cs
@@ -2,8 +2,8 @@ namespace Clients
 {
     public class Constants
     {
-        //public const string Authority = "http://idsvr.smartcityclick.com:7280";
-        public const string Authority = "https://localhost:5101";
+        public const string Authority = "http://idsvr.smartcityclick.com:7280";
+        //public const string Authority = "https://localhost:5101";
         public const string AuthorityMtls = "https://identityserver.local";
 
         public const string SampleApi = "https://localhost:5005/";
