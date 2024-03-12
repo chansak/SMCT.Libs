6c33a0a783c874412b58aa58b8838c4eefcdb08d
Chansak <chansak_noi@hotmail.com>
2024-03-12 14:18:07 +0700

    bug fixing for SameSite=None, Must be set Secure = true. The application must be run under HTTPS

--- Constants/Constants.cs
+++ Constants/Constants.cs
@@ -2,8 +2,11 @@ namespace Clients
 {
     public class Constants
     {
-        public const string Authority = "http://idsvr.smartcityclick.com:7280";
+        //public const string Authority = "http://idsvr.smartcityclick.com:7280";
         //public const string Authority = "https://localhost:5101";
+        //public const string Authority = "http://localhost:5100";
+        public const string Authority = "https://smartcityclick.com";
+        //public const string Authority = "http://dev-idsv.com:8080";
         public const string AuthorityMtls = "https://identityserver.local";
 
         public const string SampleApi = "https://localhost:5005/";
