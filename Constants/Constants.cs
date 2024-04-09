01a50a07b607efcbecbd3be4dec2432abb7803a1
Chansak <chansak_noi@hotmail.com>
2024-04-09 23:49:33 +0700

    force the app to be used https only

--- Constants/Constants.cs
+++ Constants/Constants.cs
@@ -3,9 +3,9 @@ namespace Clients
     public class Constants
     {
         //public const string Authority = "http://idsvr.smartcityclick.com:7280";
-        public const string Authority = "https://localhost:5101";
+        //public const string Authority = "https://localhost:5101";
         //public const string Authority = "http://localhost:5100";
-        //public const string Authority = "https://smartcityclick.com";
+        public const string Authority = "https://smartcityclick.com";
         //public const string Authority = "http://dev-idsv.com:8080";
         public const string AuthorityMtls = "https://identityserver.local";
 
